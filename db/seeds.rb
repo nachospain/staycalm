078390874c5c7bd9b706fc2f61efeb7fedff68e2
nacho lopez <nachete-1995@hotmail.com>
2019-01-02 18:18:02 +0100

    fixed style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating admin"
 nacho = User.new
 nacho = User.new(email:"test@test.com", password:"test@test.com")
+puts "Admin created"
